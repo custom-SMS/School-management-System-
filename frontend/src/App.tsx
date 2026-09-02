e5597d4a87273d9028450f12b5a1f0fa356a8f34
fenet-s <fenetseifudin1@gmail.com>
2026-09-02 14:00:33 -0500

    chore: remove intro animation screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,6 @@ import {
 import ProtectedRoute from "./components/ProtectedRoute";
 import { ToastContainer } from "react-toastify";
 import PWAInstallPrompt from "./components/PWAInstallPrompt";
-import MobileAppIntro from "./components/MobileAppIntro";
 
 
 // General
@@ -849,7 +848,6 @@ function App() {
                 }
               />
             </Routes>
-            <MobileAppIntro />
             <PWAInstallPrompt />
       </AcademicYearProvider>
     </Router>
